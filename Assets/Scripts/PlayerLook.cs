c17135777fc2792175efc9a8d790370a287308ad
Max Wertheim <minecart123@aol.com>
2019-10-07 17:21:40 -0400

    Player can now look in all directions.

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public string mousexInputString, mouseyInputString;
+    public float sensitivity;
+
+    public Transform playerBody;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        xAxisClamp = 0.0f;
+        LockCursor();
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mousexInputString) * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseyInputString) * sensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if(xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(-Vector3.right * mouseY);
+        playerBody.transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
