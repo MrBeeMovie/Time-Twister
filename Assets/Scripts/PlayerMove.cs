4af9fcd82867d25c6e98f9d5ef5df061843fe98f
Max Wertheim <minecart123@aol.com>
2019-10-07 23:06:53 -0400

    Fixed jumping.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,14 +7,16 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private string horizontalInputName, verticalInputName;
     [SerializeField] private float movementSpeed;
     [SerializeField] private AnimationCurve jumpFalloff;
-    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private float jumpMultiplier, gravityMultiplier;
     [SerializeField] private KeyCode jumpKey;
 
+    private Vector3 gravity;
     private bool isJumping = false;
     private CharacterController charController;
 
     private void Awake()
     {
+        gravity = new Vector3(0, gravityMultiplier * Physics.gravity.y, 0)  ;
         charController = GetComponent<CharacterController>();
     }
 
@@ -28,12 +30,13 @@ public class PlayerMove : MonoBehaviour
         float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
         float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
 
-        Vector3 forwardMovement = transform.forward * vertInput;
-        Vector3 rightMovement = transform.right * horizInput;
+        Vector3 forwardMovement = transform.forward * vertInput * Time.deltaTime;
+        Vector3 rightMovement = transform.right * horizInput * Time.deltaTime;
 
-        charController.SimpleMove(forwardMovement + rightMovement);
+        charController.Move(forwardMovement + rightMovement);
 
         JumpInput();
+        ApplyGravity();
     }
 
     private void JumpInput()
@@ -57,10 +60,19 @@ public class PlayerMove : MonoBehaviour
             float jumpForce = jumpFalloff.Evaluate(timeInAir);
             charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
             timeInAir += Time.deltaTime;
+
+            // checks if we have reached last key in animation curve
+            if (jumpForce == jumpFalloff[jumpFalloff.length - 1].value)
+                isJumping = false;
             yield return null;
-        } while (!charController.isGrounded & charController.collisionFlags != CollisionFlags.Above);
+        } while (charController.collisionFlags != CollisionFlags.Above & isJumping);
 
         charController.slopeLimit = slopeLimit;
-        isJumping = false;
+    }
+
+    private void ApplyGravity()
+    {
+        if (!charController.isGrounded & !isJumping)
+            charController.Move(gravity * Time.deltaTime);
     }
 }
