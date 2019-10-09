57eb5352d502e927ee4f1dccd9d9578e3cf93794
Max Wertheim <minecart123@aol.com>
2019-10-09 13:04:00 -0400

    Added script to controll time independant objects.

---
+++ Assets/Scripts/MoveDuringFreeze.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDuringFreeze : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
