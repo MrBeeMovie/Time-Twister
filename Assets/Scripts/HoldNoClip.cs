e16c77308a9640679207377b8a4336478a0bf9f0
Max Wertheim <minecart123@aol.com>
2019-11-07 20:21:20 -0500

    Started a test level for game.

--- Assets/Scripts/HoldNoClip.cs
+++ Assets/Scripts/HoldNoClip.cs
@@ -5,62 +5,32 @@ using UnityEngine.Animations;
 
 public class HoldNoClip : MonoBehaviour
 {
-    [SerializeField] private float weightStep = .1f, minWeight = .1f, moveDelay = 1;
+    [SerializeField] private const float MIN_WEIGHT = .1f;
 
     private ParentConstraint parentConstraint;
-    private Vector3 translationOffset, rotationOffset;
-    private bool scaleWeight = false;
-    private float timeElapsed = 0f;
 
-    private const float FULL_WEIGHT = 1f;
+    private const float FULL_WEIGHT = 1f, ZERO_WEIGHT = 0f;
 
     private void Awake()
     {
         parentConstraint = GetComponent<ParentConstraint>();
     }
 
-    private void Update()
-    {
-        // scale weight back up
-        if(scaleWeight)
-            parentConstraint.weight = Mathf.Min(parentConstraint.weight + weightStep, 1f);
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
-        // set elapsed time equal to zero
-        timeElapsed = 0;
-
-        // set scaleWeight to false
-        scaleWeight = false;
+        // set the translation at rest 
+        parentConstraint.translationAtRest = Camera.main.transform.InverseTransformPoint(transform.position);
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        // ignore collisions between the holdable and player
-        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Holdable"));
-
-        // record elapsedTime
-        timeElapsed += Time.deltaTime;
-        
-        // if the elapsed time is greater than or equal to the move delay
-        if (timeElapsed >= moveDelay)
-        {
-            // set rest position of parent constraint
-            parentConstraint.translationAtRest = parentConstraint.GetSource(0).sourceTransform.TransformPoint(Vector3.forward * .5f);
-
-            // scale down parent constraint until zero
-            if (parentConstraint.weight != minWeight)
-                parentConstraint.weight = Mathf.Max(parentConstraint.weight - weightStep, minWeight);
-        }
+        // set weight to zero when colliding with a different object
+        parentConstraint.weight = ZERO_WEIGHT;
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        // reenable collisions between the holdable and player
-        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Holdable"), true);
-
-        // set scaleWeight back to false
-        scaleWeight = true;
+        // set weight to full when no longer colliding
+        parentConstraint.weight = FULL_WEIGHT;
     }
 }
